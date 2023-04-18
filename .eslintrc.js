a0643f316432ff5bf328fbb45f1e47a802bea561
cheraa-git <chernetsov.aa@mail.ru>
2023-04-18 11:43:47 +0400

    start commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  extends: ['plugin:react/recommended', 'standard'],
   overrides: [],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react'],
