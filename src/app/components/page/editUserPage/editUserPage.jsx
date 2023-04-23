00d7c2140b189a8512a13e6f005871952ef708d6
cheraa-git <chernetsov.aa@mail.ru>
2023-04-23 19:52:50 +0400

    edit user page - back button

--- src/app/components/page/editUserPage/editUserPage.jsx
+++ src/app/components/page/editUserPage/editUserPage.jsx
@@ -3,8 +3,10 @@ import { useEffect, useState } from 'react'
 import { Spinner } from '../../ui/Spinner'
 import PropTypes from 'prop-types'
 import { getProfessionsQuery, getQualitiesQuery, getUserByIdQuery } from '../../../utils/apiQueries'
+import { useHistory } from 'react-router-dom'
 
 export const EditUserPage = ({ userId }) => {
+  const history = useHistory()
   const [user, setUser] = useState()
   const [professions, setProfessions] = useState()
   const [qualities, setQualities] = useState()
@@ -18,6 +20,10 @@ export const EditUserPage = ({ userId }) => {
   if (!user || !professions || !qualities) return <Spinner />
   return (
     <div className="container mt-5">
+      <button className="btn btn-primary" onClick={() => history.push(`/users/${userId}`)}>
+        <i className="bi bi-caret-left" />
+        Назад
+      </button>
       <div className="row">
         <div className="col-md-6 offset-md-3 shadow p-4">
           <EditUserForm user={user} professions={professions} qualities={qualities} />
