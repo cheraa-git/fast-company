dd613b777be2d2c76a59c7dced9c61ab58cd78ef
cheraa-git <chernetsov.aa@mail.ru>
2023-07-10 20:38:11 +0400

    server: signup

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,38 +1,53 @@
 const express = require('express')
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
+const { check, validationResult } = require('express-validator')
+
 const { generateUserData } = require('../utils/helpers')
 const router = express.Router({ mergeParams: true })
 const tokenService = require('../services/token.service')
 
-router.post('/signup', async (req, res) => {
-  try {
-    const { email, password } = req.body
-    const existingUser = await User.findOne({ email })
-    if (existingUser) {
+router.post('/signup', [
+  check('email', 'Некорректный email').isEmail(),
+  check('password', 'Минимальная длинна пароля 8 символов').isLength({ min: 8 }),
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
       return res.status(400).json({
         error: {
-          message: 'EMAIL_EXISTS',
-          code: 400
+          message: 'INVALID_DATA',
+          code: 400,
+          errors: errors.array()
         }
       })
     }
-    const hashedPassword = await bcrypt.hash(password, 12)
-    const newUser = await User.create({
-      ...generateUserData(),
-      ...req.body,
-      password: hashedPassword
-    })
-    const tokens = tokenService.generate({ _id: newUser._id })
-    await tokenService.save(newUser._id, tokens.refreshToken)
+    try {
+      const { email, password } = req.body
+      const existingUser = await User.findOne({ email })
+      if (existingUser) {
+        return res.status(400).json({
+          error: {
+            message: 'EMAIL_EXISTS',
+            code: 400,
+          }
+        })
+      }
+      const hashedPassword = await bcrypt.hash(password, 12)
+      const newUser = await User.create({
+        ...generateUserData(),
+        ...req.body,
+        password: hashedPassword
+      })
+      const tokens = tokenService.generate({ _id: newUser._id })
+      await tokenService.save(newUser._id, tokens.refreshToken)
 
-    res.status(201).send({ ...tokens, userId: newUser._id })
-  } catch (error) {
-    res.status(500).json({
-      message: 'На сервере произошла ошибка. Попробуйте позже'
-    })
-  }
-})
+      res.status(201).send({ ...tokens, userId: newUser._id })
+    } catch (error) {
+      res.status(500).json({
+        message: 'На сервере произошла ошибка. Попробуйте позже'
+      })
+    }
+  }])
 router.post('/signInWithPassword', async (req, res) => {
 })
 router.post('/token', async (req, res) => {
